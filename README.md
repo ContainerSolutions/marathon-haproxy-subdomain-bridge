fba73d918064155ee0d41a714cd3f5ba7d4b4228
Frank Scholten <frank@frankscholten.nl>
2015-02-17 17:48:19 +0100

    Added cron file and install script to upload all files and move them in correct place

--- README.md
+++ README.md
@@ -9,16 +9,16 @@ When deploying an app 'myapp' to Marathon we want to access it at myapp.domain.t
 How?
 ====
 
-The bridge.go program, based on Marathon's HaProxy bridge bash script
-scans the Marathon tasks API and generates HaProxy config which maps the app unto a subdomain.
-This script is called from a cronjob and writes to /tmp/ha-proxy.cfg. A second cron job replaces
-the HaProxy config if the contents are new.
+The bridge.go program uses Marathon's tasks API to regerenate HaProxy config. It is called by the haproxy.cron every minute and then the 
+refresh-haproxy script moves the config into /etc/haproxy/haproxy.cfg and reloads HaProxy.
 
-Cronjob
-=======
-```bash
-* * * * * root /usr/local/bin/haproxy-marathon-bridge $(cat /etc/haproxy-marathon-bridge/marathons) > /tmp/haproxy.cfg ; /usr/local/bin/refresh-haproxy
-```
+Install
+====
+
+Run the install-bridge.sh script to install the scripts + cron file to machines in the Mesos cluster.
+
+
+
 
 
 
---
+++ install-bridge.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Installs the Marathon HaProxy Subdomain Bridge on all nodes in the Mesos cluster
+#
+
+install_bridge() {
+  SERVER=$1
+  echo "> Installing bridge on $SERVER"
+  echo "> Transferring files"
+  scp -o StrictHostKeyChecking=no bridge jclouds@$SERVER:~/haproxy-marathon-bridge
+  scp -o StrictHostKeyChecking=no refresh-haproxy jclouds@$SERVER:~/refresh-haproxy
+  scp -o StrictHostKeyChecking=no haproxy.cron jclouds@$SERVER:~/haproxy.cron
+  echo "> Moving files to correct place"
+  ssh -o StrictHostKeyChecking=no jclouds@$SERVER 'sudo mv -f ~/haproxy-marathon-bridge /usr/local/bin/haproxy-marathon-bridge; sudo mv -f ~/refresh-haproxy /usr/local/bin/refresh-haproxy; sudo mv -f ~/haproxy.cron /etc/cron.d/haproxy.cron'
+  echo "> DONE on $SERVER"
+}
+
+echo "> Building bridge.go"
+go build bridge.go
+echo "> DONE"
+
+SERVERS="108.59.83.246 130.211.122.50 146.148.95.197 146.148.67.239 146.148.45.43"
+for SERVER in $SERVERS
+do
+  install_bridge $SERVER
+done
+
+
